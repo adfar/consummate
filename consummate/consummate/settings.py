92c36ca6fa6044b89573b836aff73c649e4b2378
Aidan Farnum <adf5092@gmail.com>
2024-10-23 15:04:36 -0400

    add hosts

--- consummate/consummate/settings.py
+++ consummate/consummate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*1xiipg(28ke%^9r+s)#bxtri#=!e)l%$g3)5b&8i3!d#u^1k9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
