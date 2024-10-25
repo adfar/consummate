0f616c30913d443b2b0473d0a213e6a922b8681c
Aidan Farnum <adf5092@gmail.com>
2024-10-24 20:08:21 -0400

    dirs back

--- consummate/consummate/settings.py
+++ consummate/consummate/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  '/opt/projects/consummate/consummate/static'
+STATIC_ROOT =  '/opt/projects/consummate/consummate/static/consummate'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 
