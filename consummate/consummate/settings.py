52bfbafd7d695b59d3880ab586d5e9a3abfc4d71
Aidan Farnum <adf5092@gmail.com>
2024-10-24 20:14:43 -0400

    simplify

--- consummate/consummate/settings.py
+++ consummate/consummate/settings.py
@@ -118,11 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =  '/opt/projects/consummate/consummate/static/consummate'
-STATICFILES_DIRS = [
-    '/opt/projects/consummate/consummate/static',
-]
-
 
 
 # Default primary key field type
